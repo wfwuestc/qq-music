dd643188576717631e3ab9dfdcb8837bbdc2d57b
Wen <wfwuestc@163.com>
2017-11-07 16:12:23 +0800

    search loading completed

--- scripts/search.js
+++ scripts/search.js
@@ -1,4 +1,5 @@
 import {log} from './slider'
+
 function SwitchCancelBtn() {
   const input = document.querySelector('#search-in')
   const cancelBtn = document.querySelector('.cancel')
@@ -9,48 +10,80 @@ function SwitchCancelBtn() {
     cancelBtn.setAttribute('style', 'display:none')
   })
 }
+
 class Search {
   constructor(el) {
     this.$el = el
     this.input = this.$el.querySelector('#search-in')
     this.keyword = ''
-    this.page = 2
+    this.page = 1
+    this.fetching = false
+    this.nomore = false
     this.input.addEventListener('keyup', this.onKeyUp.bind(this))
     this.$song = this.$el.querySelector('.song-wrap')
+    window.addEventListener('scroll', this.onScroll.bind(this))
   }
+
   onKeyUp(e) {
     let keyword = e.target.value.trim() //使用trim()去除两端空白
-    log('search:',keyword)
-    if(!keyword) {return}
-    if(e.key !== 'Enter') {return}
+    log('search:', keyword)
+    if (!keyword) {
+      this.reset()
+    }
+    if (e.key !== 'Enter') {
+      return
+    }
     this.search(keyword)
   }
 
-  search(key) {
-    this.keyword = key
-    fetch(`http://47.91.156.35:5365/search?keyword=${this.keyword}&page=${this.page}`)
-        .then(res => res.json())
-        .then(json => json.data.song.list)
-        .then(songs => this.append(songs))
+  reset() {
+    this.page = 1
+    this.keyword = ''
+    this.$song.innerHTML = ''
   }
 
-  append(songs){
+  search(key, page) {
+    if (this.fetching) return
+    document.querySelector('.loading').setAttribute('style', 'display:flex')
+    this.keyword = key
+    this.fetching = true
+    fetch(`http://47.91.156.35:5365/search?keyword=${this.keyword}&page=${page || this.page}`)
+        .then(res => res.json())
+        .then(json => {
+          this.page = json.data.song.curpage
+          this.nomore = (json.message === "no results")
+          return json.data.song.list
+        })
+        .then(songs => this.append(songs))
+        .then(() => {
+          this.fetching = false
+          document.querySelector('.loading').setAttribute('style', 'display:none')
+        })
+  }
+
+  append(songs) {
     log(songs)
     let html = songs.map(item =>
-      `
+        `
         <li class="song-item">
         <i class="music-icon"></i>
         <h6 class="song-title">${item.songname}</h6>
         <p class="singer">${item.singer.map(s => s.name).join(' ')}</p>
       </li>
-      `
+      `,
     ).join('')
-    log('html',html)
     this.$song.insertAdjacentHTML('beforeend', html)
-    this.scroll()
   }
-  scroll() {
 
+  onScroll() {
+    if (this.nomore) {
+      document.querySelector('.nomore').setAttribute('style','display:block')
+      return window.removeEventListener('scroll', this.onScroll)
+    }
+    if (document.documentElement.clientHeight + pageYOffset > document.body.scrollHeight - 1) {
+      this.search(this.keyword, this.page + 1)
+    }
   }
 }
+
 export {SwitchCancelBtn, Search}
